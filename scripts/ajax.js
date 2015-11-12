c4a7ca2096ac9a89d4c026b8cf235e4d96845fd3
Kaylal12 <kaylalei224@gmail.com>
2015-11-12 01:13:58 -0500

    Adds bases for ajax request functions.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -91,4 +91,146 @@ $(document).ready(function() {
       console.log("error");
     });
   });
+
+  // LOGOUT REQUEST
+  // $("#logout").on('click', function(event){
+    // $.ajax({
+      // method: 'DELETE',
+      // url: inspire_url + '/logout/id'
+      // headers: {
+        // Authorization: 'Token token=' + token
+      // }
+    // })
+  // });
+
+  // 'GET' /profiles REQUEST
+  // $(".explore-profiles").on('click', function(event){
+    // $.ajax({
+      // method: 'GET',
+      // url: inspire_url + '/profiles'
+    // })
+  // })
+
+  // 'GET' /images REQUEST
+  // $(".explore-photos").on('click', function(event){
+    // $.ajax({
+      // method: 'GET',
+      // url: inspire_url + '/images'
+    // })
+  // })
+
+  // 'POST' /profiles REQUEST
+  $('#create-profile').on('submit', function(e){
+    e.preventDefault();
+    var reader = new FileReader();
+
+    reader.onload = function(event){
+      $.ajax({
+        url: inspire_url + '/profiles',
+        method: 'POST',
+        data: { profile: {
+          first_name: $('#first_name').val(),
+          last_name: $('#last_name').val(),
+          description: $("#description").val(),
+          profile_picture: event.target.result,
+          // user_id: ?
+        } }
+      }).done(function(response){
+        console.log('Success');
+      }).fail(function(response){
+        console.error('Whoops!');
+      })
+    };
+
+    $fileInput = $('#profile_picture');
+    reader.readAsDataURL($fileInput[0].files[0]);
+
+  });
+
+  // 'GET' /profiles/id REQUEST
+  // $("").on('click', function(event){
+    // $.ajax({
+      // method: 'GET',
+      // url: inspire_url + '/profiles/id'
+    // })
+  // });
+
+  // 'PATCH' /profiles/id REQUEST
+  // $("").on('click', function(event){
+    // $.ajax({
+      // method: 'PATCH',
+      // url: inspire_url + '/profiles/id'
+      // headers: {
+        // Authorization: 'Token token=' + token
+      // }
+    // })
+  // });
+
+  // 'DELETE' /profiles/id REQUEST
+  // $("").on('click', function(event){
+    // $.ajax({
+      // method: 'DELETE',
+      // url: inspire_url + '/profiles/id'
+      // headers: {
+        // Authorization: 'Token token=' + token
+      // }
+    // })
+  // });
+
+  // 'POST' /images REQUEST
+  // $('#image-upload').on('submit', function(e){
+  //   e.preventDefault();
+  //   var reader = new FileReader();
+
+  //   reader.onload = function(event){
+  //     $.ajax({
+  //       url: inspire_url + '/images',
+  //       method: 'POST',
+  //       data: { images: {
+  //         caption: $('#caption').val(),
+  //         image_post: event.target.result,
+  //         // user_id: ?
+  //       } }
+  //     }).done(function(response){
+  //       console.log('Success');
+  //     }).fail(function(response){
+  //       console.error('Whoops!');
+  //     })
+  //   };
+
+  //   $fileInput = $('#profile_picture');
+  //   reader.readAsDataURL($fileInput[0].files[0]);
+
+  // });
+
+  // 'GET' /images/id REQUEST
+  // $("").on('click', function(event){
+    // $.ajax({
+      // method: 'GET',
+      // url: inspire_url + '/images/id'
+    // })
+  // });
+
+  // 'PATCH' /images/id REQUEST
+  // $("").on('click', function(event){
+    // $.ajax({
+      // method: 'PATCH',
+      // url: inspire_url + '/images/id'
+      // headers: {
+        // Authorization: 'Token token=' + token
+      // }
+    // })
+  // });
+
+  // 'DELETE' /images/id REQUEST
+  // $("").on('click', function(event){
+    // $.ajax({
+      // method: 'DELETE',
+      // url: inspire_url + '/images/id'
+      // headers: {
+        // Authorization: 'Token token=' + token
+      // }
+    // })
+  // });
+
 });
