c095436b660fd6270083a279a0fa1f055b839f74
Kaylal12 <kaylalei224@gmail.com>
2015-11-11 22:31:10 -0500

    Adds comments.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,3 +1,53 @@
+// 'use strict'
+// var inspireapi = {
+//   inspire: 'http://localhost:3000',
+
+//   register: function register(credentials, callback) {
+//     $.ajax({
+//       method: 'POST',
+//       url: this.inspire + '/register',
+//       contentType: 'application/json; charset=utf-8',
+//       data: JSON.stringify(credentials),
+//       dataType: 'json'
+//     }, callback);
+//   }
+// };
+
+// $(document).ready(function(){
+
+//   var form2object = function(form) {
+//     var data = {};
+//     $(form).children().each(function(index, element) {
+//       var type = $(this).attr('type');
+//       if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+//         data[$(this).attr('name')] = $(this).val();
+//       }
+//     });
+//     return data;
+//   };
+
+//   var wrap = function wrap(root, formData) {
+//     var wrapper = {};
+//     wrapper[root] = formData;
+//     return wrapper;
+//   };
+
+//   var callback = function callback(error, data) {
+//     if (error) {
+//       console.error(error);
+//       $('#result').val('status: ' + error.status + ', error: ' +error.error);
+//       return;
+//     }
+//     $('#result').val(JSON.stringify(data, null, 4));
+//   };
+
+//   $('#register').on('submit', function(e) {
+//     var credentials = wrap('credentials', form2object(this));
+//     inspireapi.register(credentials, callback);
+//     e.preventDefault();
+//   });
+// });
+
 $(document).ready(function() {
   var inspire_url = 'http://localhost:3000'
 
